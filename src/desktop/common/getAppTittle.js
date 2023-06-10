b06afb09f28e8c2f8e88772c2b81d226e5799428
GachiLord <name50525@gmail.com>
2023-06-11 00:13:31 +0500

    fix: incorrect app tittle on windows

--- src/desktop/common/getAppTittle.js
+++ src/desktop/common/getAppTittle.js
@@ -1,4 +1,4 @@
 module.exports = function(path, defaultTittle = 'board4you'){
-    if (path) return path.split('/').at(-1)
+    if (path) return path.split(/\/|\\/gm).at(-1)
     return defaultTittle
 }
