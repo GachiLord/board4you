695efc6d00191faa88c9bc154b2246e460c80b44
GachiLord <name50525@gmail.com>
2023-06-13 15:23:45 +0500

    test: getPathFromArgs

--- src/desktop/main/getPathFromArgs.js
+++ src/desktop/main/getPathFromArgs.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = function(args){
     for (const arg of args){
-        if (typeof arg !== 'string') continue
+        if (typeof arg !== 'string') throw new TypeError('args must be string[]')
 
         const ext = path.extname(arg).slice(1)
         if (['png', 'pdf', 'zip'].includes(ext)) return arg
