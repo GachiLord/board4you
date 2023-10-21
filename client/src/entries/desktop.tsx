cb3d67b3a12ef7e15997469d004b384c8aef31ed
Gachilord <name50525@gmail.com>
2023-10-21 16:57:54 +0500

    add esbuild as bundler for main and renderer

---
+++ client/src/entries/desktop.tsx
@@ -0,0 +1,27 @@
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import getLocalizationCfg from '../common/getLocalizationCfg';
+import store from '../renderer/store/store';
+import { Provider } from 'react-redux'
+import { LocaleContext } from '../renderer/base/constants/LocaleContext';
+import Editor from '../renderer/pages/Editor';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+// root
+const container = document.createElement('div')
+document.body.append(container)
+const root = createRoot(container)
+// render
+root.render(
+    <Provider store={store}>
+        <LocaleContext.Provider value={getLocalizationCfg(navigator.language)}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<Editor />} />
+            </Routes>
+        </BrowserRouter>
+        </LocaleContext.Provider>
+    </Provider>
+)
