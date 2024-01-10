0d0fe30810eeb9ac3dce7f74e2ee6c12d0321e68
GachiLord <name504172@gmail.com>
2024-01-11 01:19:32 +0500

    fix settings show for every board despite user is not owner

--- client/src/renderer/board/toolPanel/ToolPanel.tsx
+++ client/src/renderer/board/toolPanel/ToolPanel.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import ToolButton from "./ToolButton";
-import {BsArrowRight, BsArrowsMove, BsPen, BsEraser, BsCursor} from 'react-icons/bs'
-import {AiOutlineLine} from 'react-icons/ai'
-import {IoSquareOutline} from 'react-icons/io5'
-import {RxCircle} from 'react-icons/rx'
+import { BsArrowRight, BsArrowsMove, BsPen, BsEraser, BsCursor } from 'react-icons/bs'
+import { AiOutlineLine } from 'react-icons/ai'
+import { IoSquareOutline } from 'react-icons/io5'
+import { RxCircle } from 'react-icons/rx'
 import Persister from "../../lib/Persister";
 import store from "../../store/store";
 import ShareBar from "../ShareBar";
@@ -15,59 +15,63 @@ import { useDispatch } from "react-redux";
 import { set } from "../../features/tool";
 
 
-export default function ToolPanel(){
-    const { roomId } = useParams()
-    const isAuthor = IsOwned(roomId)
-    const dispatch = useDispatch()
+export default function ToolPanel() {
+  const { roomId } = useParams()
+  const isAuthor = IsOwned(roomId)
+  const dispatch = useDispatch()
 
-    useEffect( () => {
-        new Persister(store, 'toolSettings')
-    }, [] )
+  useEffect(() => {
+    new Persister(store, 'toolSettings')
+  }, [])
 
-    useEffect( () => {
-        if (!isAuthor) dispatch(set('move'))
-    } )
+  useEffect(() => {
+    if (!isAuthor) dispatch(set('move'))
+  })
 
-    return (
-        <div className="d-flex align-items-center flex-column" style={{"overflow": "auto"}}>
-                <ToolButton name="move">
-                    <BsArrowsMove />
-                </ToolButton>
-                {
-                    isAuthor && (
-                    <>
-                    <ToolButton name="select">
-                        <BsCursor />
-                    </ToolButton>
-                    <ToolButton name="pen" customizable>
-                        <BsPen />
-                    </ToolButton>
-                    <ToolButton name="line" customizable>
-                        <AiOutlineLine />
-                    </ToolButton>
-                    <ToolButton name="arrow" customizable>
-                        <BsArrowRight />
-                    </ToolButton>
-                    <ToolButton name="rect" customizable>
-                        <IoSquareOutline />
-                    </ToolButton>
-                    <ToolButton name="ellipse" customizable>
-                        <RxCircle />
-                    </ToolButton>
-                    <ToolButton 
-                        name="eraser"
-                        customizable
-                        hideColorPicker
-                        hideLineTypePicker
-                    >
-                        <BsEraser />
-                    </ToolButton>
-                    </>
-                    )
-                }
-                <Hr />
-                {isAuthor && <ShareBar />}
-                <Settings />
-            </div>
-    )
+  return (
+    <div className="d-flex align-items-center flex-column" style={{ "overflow": "auto" }}>
+      <ToolButton name="move">
+        <BsArrowsMove />
+      </ToolButton>
+      {
+        isAuthor && (
+          <>
+            <ToolButton name="select">
+              <BsCursor />
+            </ToolButton>
+            <ToolButton name="pen" customizable>
+              <BsPen />
+            </ToolButton>
+            <ToolButton name="line" customizable>
+              <AiOutlineLine />
+            </ToolButton>
+            <ToolButton name="arrow" customizable>
+              <BsArrowRight />
+            </ToolButton>
+            <ToolButton name="rect" customizable>
+              <IoSquareOutline />
+            </ToolButton>
+            <ToolButton name="ellipse" customizable>
+              <RxCircle />
+            </ToolButton>
+            <ToolButton
+              name="eraser"
+              customizable
+              hideColorPicker
+              hideLineTypePicker
+            >
+              <BsEraser />
+            </ToolButton>
+          </>
+        )
+      }
+      {isAuthor && (
+        <>
+          <Hr />
+          <ShareBar />
+          <Settings />
+        </>
+      )}
+    </div>
+  )
 }
