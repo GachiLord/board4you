1cc2c6d768bcce75b921bb868f4df128a1ed322e
GachiLord <name504172@gmail.com>
2024-01-10 23:38:52 +0500

    add moving by right mouse button

--- client/src/renderer/board/canvas/mouse/events/mouseDown.ts
+++ client/src/renderer/board/canvas/mouse/events/mouseDown.ts
@@ -1,7 +1,7 @@
 import { itemIn, whenDraw } from "../../../../lib/twiks";
 import { setDrawable, setDrawingShapeId } from "../../../../features/stage";
 import { emptyUndone } from '../../../../features/history'
-import {v4 as uuid4} from 'uuid'
+import { v4 as uuid4 } from 'uuid'
 import store from "../../../../store/store";
 import CanvasUtils from "../../../../lib/CanvasUtils";
 import Konva from "konva";
@@ -14,136 +14,141 @@ import BoardManager from "../../../../lib/BoardManager/BoardManager";
 
 
 
-export default function(e: KonvaEventObject<MouseEvent|TouchEvent>, boardManager: BoardManager, props: IDrawerProps){
-    // start drawing
-    store.dispatch(setDrawable(true))
-    //shape style vars
-    const state = store.getState()
-    const tool = props.tool
-    const color = props.color
-    const lineSize = props.lineSize
-    const lineType = props.lineType
-    const isDraggable = state.stage.isDraggable
-    const isShared = state.board.mode === 'shared'
-    const private_id = state.rooms[boardManager.status.roomId ? boardManager.status.roomId : "none"]
+export default function(e: KonvaEventObject<MouseEvent | TouchEvent>, boardManager: BoardManager, props: IDrawerProps) {
+  // start drawing
+  store.dispatch(setDrawable(true))
+  //shape style vars
+  const state = store.getState()
+  const tool = props.tool
+  const color = props.color
+  const lineSize = props.lineSize
+  const lineType = props.lineType
+  const isDraggable = state.stage.isDraggable
+  const isShared = state.board.mode === 'shared'
+  const private_id = state.rooms[boardManager.status.roomId ? boardManager.status.roomId : "none"]
 
 
-    whenDraw( e, boardManager, ({stage, pos, canvas, temporary}) => {
-        const share = (edit: IShape) => {
-            if (isShared && private_id && boardManager.status.roomId)
-            boardManager.send('PushSegment', {
-                public_id: boardManager.status.roomId,
-                private_id: private_id,
-                action_type: 'Start',
-                data: JSON.stringify(edit)
-            })
-        }
+  whenDraw(e, boardManager, ({ stage, pos, canvas, temporary, isRightClick }) => {
+    const share = (edit: IShape) => {
+      if (isShared && private_id && boardManager.status.roomId)
+        boardManager.send('PushSegment', {
+          public_id: boardManager.status.roomId,
+          private_id: private_id,
+          action_type: 'Start',
+          data: JSON.stringify(edit)
+        })
+    }
 
-        const undone = store.getState().history.undone.at(-1)
-        // empty undone if it exists and tool is not select
-        if (undone && tool !== 'select' && tool !== 'move') {
-            if (isShared && private_id && boardManager.status.roomId) boardManager.send('Empty', {
-                public_id: boardManager.status.roomId,
-                private_id: private_id,
-                action_type: 'undone'
-            })
-            store.dispatch(emptyUndone())
-        }
-        Selection.destroy(canvas)
-        // create shape
-        let shape: IShape|null = null
-        
-        // create shape considering the tool
-        if (itemIn(tool, 'pen', 'eraser', 'line', 'arrow')){
+    const undone = store.getState().history.undone.at(-1)
+    // empty undone if it exists and tool is not select
+    if (undone && tool !== 'select' && tool !== 'move' && !isRightClick) {
+      if (isShared && private_id && boardManager.status.roomId) boardManager.send('Empty', {
+        public_id: boardManager.status.roomId,
+        private_id: private_id,
+        action_type: 'undone'
+      })
+      store.dispatch(emptyUndone())
+    }
+    else if (isRightClick) {
+      // start dragging if right clicked
+      stage.startDrag()
+      return
+    }
+    Selection.destroy(canvas)
+    // create shape
+    let shape: IShape | null = null
 
-            shape = {
-                tool: tool,
-                points: [pos.x, pos.y],
-                type: tool === 'arrow' ? 'arrow' : 'line',
-                color: tool !== 'eraser' ? color: '#ffffff',
-                shapeId: uuid4(),
-                x: 0,
-                y: 0,
-                lineSize: lineSize,
-                lineType: lineType,
-                connected: []
-            }
+    // create shape considering the tool
+    if (itemIn(tool, 'pen', 'eraser', 'line', 'arrow')) {
 
-            // add shape to canvas
-            canvas.add(CanvasUtils.toKonvaObject(shape))
-            // save active shapeId
-            store.dispatch(setDrawingShapeId(shape.shapeId))
-            // send segment
-            share(shape)
-        }
-        else if (tool === 'rect'){
-            shape = {
-                tool: tool,
-                type: tool,
-                x: pos.x,
-                y: pos.y,
-                height: 0,
-                width: 0,
-                color: color,
-                shapeId: uuid4(),
-                lineSize: lineSize,
-                lineType: lineType,
-            }
-            canvas.add(CanvasUtils.toKonvaObject(shape))
-            // save active shapeId
-            store.dispatch(setDrawingShapeId(shape.shapeId))
-            // send segment
-            share(shape)
-        }
-        else if (tool === 'ellipse'){
-            shape = {
-                tool: tool,
-                type: tool,
-                x: pos.x,
-                y: pos.y,
-                radiusY: 0,
-                radiusX: 0,
-                color: color,
-                shapeId: uuid4(),
-                lineSize: lineSize,
-                lineType: lineType,
-            }
-            canvas.add(CanvasUtils.toKonvaObject(shape))
-            // save active shapeId
-            store.dispatch(setDrawingShapeId(shape.shapeId))
-            // send segment
-            share(shape)
-        }
-        else if (tool === 'select' && !isDraggable && e.target.attrs.id !== 'selectRect'){
-            // select multiple shapes
-            if ( e.target === stage ){
-                const shape = new Konva.Rect({
-                    x: pos.x, 
-                    y: pos.y,
-                    height: 0,
-                    width: 0,
-                    stroke: primaryColor,
-                    strokeWidth:2,
-                    opacity:0.8,
-                    dash:[5, 5],
-                    id:'selectRect',
-                    shadowForStrokeEnabled:false,
-                })
+      shape = {
+        tool: tool,
+        points: [pos.x, pos.y],
+        type: tool === 'arrow' ? 'arrow' : 'line',
+        color: tool !== 'eraser' ? color : '#ffffff',
+        shapeId: uuid4(),
+        x: 0,
+        y: 0,
+        lineSize: lineSize,
+        lineType: lineType,
+        connected: []
+      }
 
-                temporary.add(shape)
-            }
-            // select only clicked shape
-            else {
-                const connected: Konva.Shape[] = []
-                // find shapes which have interception with clientRect
-                canvas.children.forEach(s => {
-                    if ( s.attrs.connected.has(e.target.attrs.shapeId) && s instanceof Konva.Shape) connected.push(s)
-                })
-                // create transformer for them
-                if (e.target instanceof Konva.Shape) Selection.create([e.target, ...connected], boardManager)
-            }
-        }
-    } )
-    
-    
+      // add shape to canvas
+      canvas.add(CanvasUtils.toKonvaObject(shape))
+      // save active shapeId
+      store.dispatch(setDrawingShapeId(shape.shapeId))
+      // send segment
+      share(shape)
+    }
+    else if (tool === 'rect') {
+      shape = {
+        tool: tool,
+        type: tool,
+        x: pos.x,
+        y: pos.y,
+        height: 0,
+        width: 0,
+        color: color,
+        shapeId: uuid4(),
+        lineSize: lineSize,
+        lineType: lineType,
+      }
+      canvas.add(CanvasUtils.toKonvaObject(shape))
+      // save active shapeId
+      store.dispatch(setDrawingShapeId(shape.shapeId))
+      // send segment
+      share(shape)
+    }
+    else if (tool === 'ellipse') {
+      shape = {
+        tool: tool,
+        type: tool,
+        x: pos.x,
+        y: pos.y,
+        radiusY: 0,
+        radiusX: 0,
+        color: color,
+        shapeId: uuid4(),
+        lineSize: lineSize,
+        lineType: lineType,
+      }
+      canvas.add(CanvasUtils.toKonvaObject(shape))
+      // save active shapeId
+      store.dispatch(setDrawingShapeId(shape.shapeId))
+      // send segment
+      share(shape)
+    }
+    else if (tool === 'select' && !isDraggable && e.target.attrs.id !== 'selectRect') {
+      // select multiple shapes
+      if (e.target === stage) {
+        const shape = new Konva.Rect({
+          x: pos.x,
+          y: pos.y,
+          height: 0,
+          width: 0,
+          stroke: primaryColor,
+          strokeWidth: 2,
+          opacity: 0.8,
+          dash: [5, 5],
+          id: 'selectRect',
+          shadowForStrokeEnabled: false,
+        })
+
+        temporary.add(shape)
+      }
+      // select only clicked shape
+      else {
+        const connected: Konva.Shape[] = []
+        // find shapes which have interception with clientRect
+        canvas.children.forEach(s => {
+          if (s.attrs.connected.has(e.target.attrs.shapeId) && s instanceof Konva.Shape) connected.push(s)
+        })
+        // create transformer for them
+        if (e.target instanceof Konva.Shape) Selection.create([e.target, ...connected], boardManager)
+      }
+    }
+  })
+
+
 }
--- client/src/renderer/board/canvas/mouse/events/mouseMove.ts
+++ client/src/renderer/board/canvas/mouse/events/mouseMove.ts
@@ -10,111 +10,112 @@ import BoardManager from "../../../../lib/BoardManager/BoardManager";
 import CanvasUtils from "../../../../lib/CanvasUtils";
 import { ShareData } from "../../share/ShareData";
 
-export default function(e: KonvaEventObject<MouseEvent|TouchEvent>, boardManager: BoardManager, props: IDrawerProps){
-    const tool = props.tool
-    const state = store.getState()
-    const drawingShapeId = store.getState().stage.drawingShapeId
-    const isDrawable = state.stage.isDrawable
-    const isShared = state.board.mode === 'shared'
-    const private_id = state.rooms[boardManager.status.roomId]
-    
-    const share = (data: ShareData) => {
-        if (isShared)
-        boardManager.send('PushSegment', {
-            public_id: boardManager.status.roomId,
-            private_id: private_id,
-            action_type: 'Update',
-            data: JSON.stringify(data)
+export default function(e: KonvaEventObject<MouseEvent | TouchEvent>, boardManager: BoardManager, props: IDrawerProps) {
+  const tool = props.tool
+  const state = store.getState()
+  const drawingShapeId = store.getState().stage.drawingShapeId
+  const isDrawable = state.stage.isDrawable
+  const isShared = state.board.mode === 'shared'
+  const private_id = state.rooms[boardManager.status.roomId]
+
+  const share = (data: ShareData) => {
+    if (isShared)
+      boardManager.send('PushSegment', {
+        public_id: boardManager.status.roomId,
+        private_id: private_id,
+        action_type: 'Update',
+        data: JSON.stringify(data)
+      })
+  }
+  whenDraw(e, boardManager, ({ stage, pos, canvas, temporary, isRightClick }) => {
+    // do nothing if right mouse clicked
+    if (isRightClick) return
+    // handle tools usage
+    if (itemIn(tool, 'pen', 'eraser') && isDrawable && drawingShapeId) {
+      const target = e.target
+      const lastline: unknown = CanvasUtils.findLastOne(canvas, { shapeId: drawingShapeId })
+      // validate lastLine
+      if (!(lastline instanceof Line)) throw new TypeError('last created element must be a Line')
+      // add ref to eraser line if pointer is on shape
+      if (target !== stage && tool === 'eraser') {
+        // add connected
+        lastline.attrs.connected.add(target.attrs.shapeId)
+        // send segments
+        share({
+          shapeId: target.attrs.shapeId,
+          connected: drawingShapeId
         })
+      }
+      // add points
+      lastline.points(lastline.attrs.points.concat([pos.x, pos.y]))
+      // send segments
+      share({
+        shapeId: drawingShapeId,
+        addPoints: [pos.x, pos.y]
+      })
     }
 
-    whenDraw( e, boardManager, ({stage, pos, canvas, temporary}) => {
-        // handle tools usage
-        if (itemIn(tool, 'pen', 'eraser') && isDrawable && drawingShapeId){
-            const target = e.target
-            const lastline: unknown = CanvasUtils.findLastOne(canvas, { shapeId: drawingShapeId })
-            // validate lastLine
-            if (!(lastline instanceof Line)) throw new TypeError('last created element must be a Line')
-            // add ref to eraser line if pointer is on shape
-            if (target !== stage && tool === 'eraser'){
-                // add connected
-                lastline.attrs.connected.add(target.attrs.shapeId)
-                // send segments
-                share({
-                    shapeId: target.attrs.shapeId,
-                    connected: drawingShapeId
-                })
-            }
-            // add points
-            lastline.points(lastline.attrs.points.concat([pos.x, pos.y]))
-            // send segments
-            share({
-                shapeId: drawingShapeId,
-                addPoints: [pos.x, pos.y]
-            })
-        }
-        
-        else if (itemIn(tool, 'arrow', 'line') && isDrawable && drawingShapeId){
-            const lastLine: unknown = canvas.children.at(-1)
-            // validate
-            if (!(lastLine instanceof Line || lastLine instanceof Arrow)) throw new TypeError('last created element must be a Line or an Arrow')
-            // set points
-            if (lastLine.attrs.points.length > 2) lastLine.points(lastLine.attrs.points.slice(0,2))
-            // add points
-            lastLine.points(lastLine.attrs.points.concat([pos.x, pos.y]))
-            // send segments
-            share({
-                shapeId: drawingShapeId,
-                points: [pos.x, pos.y]
-            })
-        }
-        else if (tool === 'rect' && isDrawable && drawingShapeId){
-            const shape: unknown = canvas.children.at(-1)
-            // validate
-            if (!(shape instanceof Rect)) throw new TypeError('last created element must be a Rect')
-            // prepare new values
-            const newWidth = pos.x - shape.attrs.x
-            const newHeight = pos.y - shape.attrs.y
-            // update
-            shape.setAttrs({
-                width: newWidth,
-                height: newHeight
-            })
-            // send segments
-            share({
-                shapeId: drawingShapeId,
-                width: newWidth,
-                height: newHeight
-            })
-        }
-        else if (tool === 'ellipse' && isDrawable && drawingShapeId){
-            const shape: unknown = canvas.children.at(-1)
-            // validate
-            if (!(shape instanceof Ellipse)) throw new TypeError('last created element must be an Ellipse')
-            // prepare values
-            const newRadiusX = Math.abs(pos.x - shape.attrs.x)
-            const newRadiusY = Math.abs(pos.y - shape.attrs.y)
-            // update
-            shape.setAttrs({
-                radiusX: newRadiusX,
-                radiusY: newRadiusY
-            })
-            // send segments
-            share({
-                shapeId: drawingShapeId,
-                radiusX: newRadiusX,
-                radiusY: newRadiusY
-            })
-        }
-        else if (tool === 'select' && isDrawable && temporary.children[0]){
-            const shape: unknown = temporary.children[0]
-            // validate
-            if (!(shape instanceof Rect)) throw new TypeError('last created element must be a Rect')
-            // update
-            shape.setAttrs({
-                width: pos.x - shape.attrs.x,
-                height: pos.y - shape.attrs.y
-            })
-        }
-    } )
+    else if (itemIn(tool, 'arrow', 'line') && isDrawable && drawingShapeId) {
+      const lastLine: unknown = canvas.children.at(-1)
+      // validate
+      if (!(lastLine instanceof Line || lastLine instanceof Arrow)) throw new TypeError('last created element must be a Line or an Arrow')
+      // set points
+      if (lastLine.attrs.points.length > 2) lastLine.points(lastLine.attrs.points.slice(0, 2))
+      // add points
+      lastLine.points(lastLine.attrs.points.concat([pos.x, pos.y]))
+      // send segments
+      share({
+        shapeId: drawingShapeId,
+        points: [pos.x, pos.y]
+      })
+    }
+    else if (tool === 'rect' && isDrawable && drawingShapeId) {
+      const shape: unknown = canvas.children.at(-1)
+      // validate
+      if (!(shape instanceof Rect)) throw new TypeError('last created element must be a Rect')
+      // prepare new values
+      const newWidth = pos.x - shape.attrs.x
+      const newHeight = pos.y - shape.attrs.y
+      // update
+      shape.setAttrs({
+        width: newWidth,
+        height: newHeight
+      })
+      // send segments
+      share({
+        shapeId: drawingShapeId,
+        width: newWidth,
+        height: newHeight
+      })
+    }
+    else if (tool === 'ellipse' && isDrawable && drawingShapeId) {
+      const shape: unknown = canvas.children.at(-1)
+      // validate
+      if (!(shape instanceof Ellipse)) throw new TypeError('last created element must be an Ellipse')
+      // prepare values
+      const newRadiusX = Math.abs(pos.x - shape.attrs.x)
+      const newRadiusY = Math.abs(pos.y - shape.attrs.y)
+      // update
+      shape.setAttrs({
+        radiusX: newRadiusX,
+        radiusY: newRadiusY
+      })
+      // send segments
+      share({
+        shapeId: drawingShapeId,
+        radiusX: newRadiusX,
+        radiusY: newRadiusY
+      })
+    }
+    else if (tool === 'select' && isDrawable && temporary.children[0]) {
+      const shape: unknown = temporary.children[0]
+      // validate
+      if (!(shape instanceof Rect)) throw new TypeError('last created element must be a Rect')
+      // update
+      shape.setAttrs({
+        width: pos.x - shape.attrs.x,
+        height: pos.y - shape.attrs.y
+      })
+    }
+  })
 }
