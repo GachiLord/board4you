208346d7322e9e412bfc84d0a021463d4c936eae
GachiLord <name504172@gmail.com>
2024-01-19 16:46:22 +0500

    fix bug when modify edits don't sync

--- client/scripts/buildWeb.sh
+++ client/scripts/buildWeb.sh
@@ -1,2 +1,4 @@
-cp ./node_modules/pdfjs-dist/build/pdf.worker.js ./public/pdf.worker.js; 
+cp ./node_modules/pdfjs-dist/build/pdf.worker.js ./public/pdf.worker.js
+cp ./build/icon.ico ./public/favicon.ico
 ./node_modules/.bin/esbuild ./src/entries/web.tsx --bundle --outdir=./public --loader:.png=dataurl --minify
+
--- client/src/renderer/board/canvas/mouse/func/shapeChange.ts
+++ client/src/renderer/board/canvas/mouse/func/shapeChange.ts
@@ -4,7 +4,7 @@ import { addCurrent, emptyUndone } from "../../../../features/history";
 import { setSelection } from "../../../../features/select";
 import Konva from "konva";
 import IShape from "../../../../base/typing/IShape";
-import { run } from "../../../../lib/twiks";
+import { itemIn, run } from "../../../../lib/twiks";
 import { v4 } from "uuid";
 import { Edit } from "../../../../lib/EditManager";
 import BoardManager from "../../../../lib/BoardManager/BoardManager";
@@ -12,77 +12,77 @@ import { convertToStrings } from "../../share/convert";
 
 
 
-export default function(transformer: Konva.Transformer, boardManager: BoardManager){
-    // empty undone
-    if (store.getState().history.undone.length !== 0) store.dispatch(emptyUndone())
+export default function(transformer: Konva.Transformer, boardManager: BoardManager) {
+  // empty undone
+  if (store.getState().history.undone.length !== 0) store.dispatch(emptyUndone())
 
-    transformer.on('dragstart transformstart', () => {
-        changeHandler(transformer, boardManager)
-    })
+  transformer.on('dragstart transformstart', () => {
+    changeHandler(transformer, boardManager)
+  })
 }
 
 
-async function changeHandler(transformer: Konva.Transformer, boardManager: BoardManager){
-    // boardmanager vars
-    const share = (edit: Edit) => {
-        const state = store.getState()
-        const shared = state.board.mode === 'shared'
-        const public_id = boardManager.status.roomId
-        const private_id: undefined|string = state.rooms[public_id]
+async function changeHandler(transformer: Konva.Transformer, boardManager: BoardManager) {
+  // boardmanager vars
+  const share = (edit: Edit) => {
+    const state = store.getState()
+    const shared = itemIn(state.board.mode, 'coop', 'author')
+    const public_id = boardManager.status.roomId
+    const private_id: undefined | string = state.rooms[public_id]
 
-        if (shared){
-            boardManager.send('Empty', {
-                public_id,
-                private_id,
-                action_type: 'undone'
-            })
-            boardManager.send('Push', {
-                public_id,
-                private_id,
-                data: convertToStrings([edit]),
-                silent: false
-            })
-        }
+    if (shared) {
+      boardManager.send('Empty', {
+        public_id,
+        private_id,
+        action_type: 'undone'
+      })
+      boardManager.send('Push', {
+        public_id,
+        private_id,
+        data: convertToStrings([edit]),
+        silent: false
+      })
     }
-    // transform
-    const shapes = transformer.nodes()
-    const initial: IShape[] = shapes.map( shape => {
-        if (shape instanceof Konva.Shape){
-            return CanvasUtils.toShape(shape)
-        }
-    } )
-    const current: IShape[] = []
+  }
+  // transform
+  const shapes = transformer.nodes()
+  const initial: IShape[] = shapes.map(shape => {
+    if (shape instanceof Konva.Shape) {
+      return CanvasUtils.toShape(shape)
+    }
+  })
+  const current: IShape[] = []
 
-    // add current attrs
-    transformer.on('dragend transformend', () => {
-        transformer.off('dragend transformend')
-        // update selection
-        store.dispatch(setSelection(shapes.map( s => {
-            if (s instanceof Konva.Shape){
-                return CanvasUtils.toShape(s)
-            }
-        } )))
+  // add current attrs
+  transformer.on('dragend transformend', () => {
+    transformer.off('dragend transformend')
+    // update selection
+    store.dispatch(setSelection(shapes.map(s => {
+      if (s instanceof Konva.Shape) {
+        return CanvasUtils.toShape(s)
+      }
+    })))
 
-        // add mods
-        shapes.forEach(shape => {
-            if (shape instanceof Konva.Shape){
-                current.push(CanvasUtils.toShape(shape))
-            }
-        })
-
-        // add changes in history
-        const edit: Edit = {
-            id: v4(),
-            type: 'modify',
-            initial: initial,
-            current: current
-        }
-        store.dispatch(addCurrent(edit))
-        // send changes
-        share(edit)
-        // hadnle file change
-        run( api => {
-            api.handleFileChange()
-        } )
+    // add mods
+    shapes.forEach(shape => {
+      if (shape instanceof Konva.Shape) {
+        current.push(CanvasUtils.toShape(shape))
+      }
     })
+
+    // add changes in history
+    const edit: Edit = {
+      id: v4(),
+      type: 'modify',
+      initial: initial,
+      current: current
+    }
+    store.dispatch(addCurrent(edit))
+    // send changes
+    share(edit)
+    // hadnle file change
+    run(api => {
+      api.handleFileChange()
+    })
+  })
 }
