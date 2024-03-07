e2c38c98c17d03de7f54cc0e78891216de6157f3
GachiLord <name504172@gmail.com>
2024-03-07 21:49:29 +0500

    remove useless lines

--- client/src/renderer/board/canvas/native/handleNativeEvents.ts
+++ client/src/renderer/board/canvas/native/handleNativeEvents.ts
@@ -5,25 +5,25 @@ import Konva from "konva"
 import BoardManager from "../../../lib/BoardManager/BoardManager"
 
 
-interface props{
-    stage: Konva.Stage,
-    boardManager: BoardManager
+interface props {
+  stage: Konva.Stage,
+  boardManager: BoardManager
 }
 
 
-export default function HandleNativeEvents({ stage, boardManager }: props){
-    let keyBindingsSub: () => void = null
-    run( electron => {
-        electron.onMenuButtonClick( (_, o, d) => runCommand(stage, boardManager, o, d) )
-    },
+export default function HandleNativeEvents({ stage, boardManager }: props) {
+  let keyBindingsSub: () => void = null
+  run(electron => {
+    electron.onMenuButtonClick((_, o, d) => runCommand(stage, boardManager, o, d))
+  },
     () => {
-        keyBindingsSub = tinykeys(window, {
-            'Control+Shift+L': () => runCommand(stage, boardManager, 'selectSize'),
-            'Control+Z': () => runCommand(stage, boardManager, 'undo'),
-            'Control+Y': () => runCommand(stage, boardManager, 'redo'),
-            'Delete': () => runCommand(stage, boardManager, 'del'),
-        })
+      keyBindingsSub = tinykeys(window, {
+        'Control+Shift+L': () => runCommand(stage, boardManager, 'selectSize'),
+        'Control+Z': () => runCommand(stage, boardManager, 'undo'),
+        'Control+Y': () => runCommand(stage, boardManager, 'redo'),
+        'Delete': () => runCommand(stage, boardManager, 'del'),
+      })
     })
 
-    return window.electronAPI ? window.electronAPI.removeAllListeners : keyBindingsSub
+  return window.electronAPI ? window.electronAPI.removeAllListeners : keyBindingsSub
 }
